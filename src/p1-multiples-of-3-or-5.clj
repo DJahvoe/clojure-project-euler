349b3b9c8d548f25ee1dfc3677017c6dae8298ac
satria-mgt <satria@marimo-gt.co.jp>
2024-07-19 11:33:17 +0900

    add comment

--- src/p1-multiples-of-3-or-5.clj
+++ src/p1-multiples-of-3-or-5.clj
@@ -1,14 +1,17 @@
 (ns p1-multiples-of-3-or-5)
 
+;; Normal
 (reduce + (filter
            (fn [x] (or (= (mod x 3) 0) (= (mod x 5) 0)))
            (take 1000 (range))))
 
+;; Macro expand
 (macroexpand-1 '(->> (range)
                      (take 1000)
                      (filter (fn [x] (or (= (mod x 3) 0) (= (mod x 5) 0))))
                      (reduce +)))
 
+;; Thread last
 (->> (range)
      (take 1000)
      (filter (fn [x] (or (= (mod x 3) 0) (= (mod x 5) 0))))
